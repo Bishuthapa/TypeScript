8177bea8c9c990ad6e5b2b413d296fc1e40f7d1c
Bishuthapa <thapabishu435@gmail.com>
2025-02-24 23:57:10 +0545

    initial commit

---
+++ Array.js
@@ -0,0 +1,4 @@
+var hub = ["A", "B", "C", "D", "E"];
+var google = [23, 45, 5, 56];
+google.push(345);
+console.log(google);
---
+++ Array.ts
@@ -0,0 +1,7 @@
+const hub:string[] = ["A", "B", "C", "D", "E"];
+const google: number[] = [23,45,5,56];
+
+
+google.push(345);
+console.log(google);
+
