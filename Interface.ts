b0ad4826dea506a83409136fface2897a705308d
Bishuthapa <thapabishu435@gmail.com>
2025-03-01 23:57:14 +0545

    interface

--- Interface.ts
+++ Interface.ts
@@ -1,5 +1,22 @@
-interface football {
-    name: string;
-    color :"white"
+interface ball {
+    name: string
+    color: "white" | "black";
 
+}
+
+interface character extends ball {
+    size: "small" | "medium" | "large";
+}
+
+const football: character = {
+    name: "football",
+    color: "black",
+    size: "medium"
+
+}
+
+for (const key in football) {
+    if (football.hasOwnProperty(key)) {
+        console.log(`${key}: ${football[key as keyof character]}`);
+    }
 }
