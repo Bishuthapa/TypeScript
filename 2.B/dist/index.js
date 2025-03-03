54de203ba17ebdc744ed2978da5bcfa5b3cefbd7
Bishuthapa <thapabishu435@gmail.com>
2025-03-03 15:46:20 +0545

    class and object is ts

--- 2.B/dist/index.js
+++ 2.B/dist/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("bishesh");
+// class User {
+//     name: string;
+//     age: number;
+//     isMarried: boolean;
+//     constructor(name:string,age:number,isMarried:boolean){
+//         this.name = name;
+//         this.age = age;
+//         this.isMarried = isMarried;
+//     }
+// }
+// let bibit = new User("bibit", 17, false);
+// console.log(bibit);
+class User {
+    constructor(name, age, isMarried) {
+        this.name = name;
+        this.age = age;
+        this.isMarried = isMarried;
+    }
+}
+const bibit = new User("bibit", 17, false);
+console.log(bibit);
--- 2.B/src/index.ts
+++ 2.B/src/index.ts
@@ -1,1 +1,25 @@
-console.log("bishesh");
+// class User {
+//     name: string;
+//     age: number;
+//     isMarried: boolean;
+
+//     constructor(name:string,age:number,isMarried:boolean){
+//         this.name = name;
+//         this.age = age;
+//         this.isMarried = isMarried;
+//     }
+// }
+
+// let bibit = new User("bibit", 17, false);
+// console.log(bibit);
+
+
+class User {
+    constructor(
+        public name: string,
+        public age:Number,
+        public isMarried:Boolean) {
+    }
+}
+const bibit = new User("bibit", 17, false);
+console.log(bibit);
