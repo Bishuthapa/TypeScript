7a08724956aad4056b9421af44aad2d36456b861
Bishu Thapa <thapabishu435@gmail.com>
2025-12-18 23:16:22 +0545

    Any and Unknoown

---
+++ ts/dist/AnyAndUnknown.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let List;
+List = 12;
+List = 3.4;
+List = "List of number";
+List = true;
+List.toString();
+let topList;
+topList = 12;
+topList = 3.4;
+topList = "List of number";
+topList = true;
+//topList.toString();// it throw error 
+if (typeof topList === "string") {
+    topList.toString();
+}
+//# sourceMappingURL=AnyAndUnknown.js.map
