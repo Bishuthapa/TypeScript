d76e7ee93df660bd629f929c56c015b90ead8035
Bishu Thapa <thapabishu435@gmail.com>
2025-12-18 22:12:51 +0545

    union and any

--- ts/src/unionAndAny.ts
+++ ts/src/unionAndAny.ts
@@ -1,4 +1,4 @@
-let apiRequest : "success" | "failed" | "pending"= "pending"; //example of union 
+let apiRequest: "success" | "failed" | "pending" = "pending"; //example of union 
 
 apiRequest = "success";
 
@@ -11,14 +11,54 @@ const foods = ["dhalBhat", "tarkari", "Masu", "kolChiura"];
 
 let Foodorder: string | undefined;
 
-for(let food of foods){
-    if(food === "Masu"){
+for (let food of foods) {
+    if (food === "Masu") {
         Foodorder = food;
         break
-    }  
+    }
 };
 
 //console.log(Foodorder);
 
 
+class coffee {
+    serve() {
+        return `return balck coffee`
+    }
+}
 
+class Tea {
+    serve() {
+        return `return balck tea`
+    }
+}
+
+
+function serve(lunch: coffee | Tea) {
+    if (lunch instanceof coffee) {
+        return lunch.serve();
+    }
+}
+
+type anime = { type: "anime"; time: number };
+type liveAction = { type: "real"; time: number };
+type movie = { type: "fiction"; time: number };
+
+type films = anime | liveAction | movie;
+
+function MakeFilms(myMovies: films) {
+    switch (myMovies.type) {
+        case "anime":
+            return `This is anime`
+            break;
+
+        case "real":
+            return `This is liveAction`
+            break;
+
+        case "fiction":
+            return `This is movie`
+            break;
+    }
+
+}
