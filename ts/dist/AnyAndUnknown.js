1a06d4728210c76cd52d52b9dd2cf94847deffb8
Bishu Thapa <thapabishu435@gmail.com>
2025-12-18 23:59:51 +0545

    Never

--- ts/dist/AnyAndUnknown.js
+++ ts/dist/AnyAndUnknown.js
@@ -15,4 +15,26 @@ topList = true;
 if (typeof topList === "string") {
     topList.toString();
 }
+try {
+}
+catch (error) {
+    if (error instanceof Error) {
+        console.error(error.message);
+    }
+    //console.error(error.message)  //[it will throw error => it not sure the error has a message]
+}
+function redirectBasedOnRole(role) {
+    if (role === "Admin") {
+        console.log(`Redirectiong to the admin dashbord`);
+        return;
+    }
+    if (role === "User") {
+        console.log(`Redirectiong to the User dashbord`);
+        return;
+    }
+    role;
+}
+function neverReturn() {
+    while (true) { }
+}
 //# sourceMappingURL=AnyAndUnknown.js.map
