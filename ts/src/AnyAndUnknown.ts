1e4bd6cac6051d526ee28e3cc87668e1b886c75c
Bishu Thapa <thapabishu435@gmail.com>
2025-12-18 23:59:09 +0545

    Never

--- ts/src/AnyAndUnknown.ts
+++ ts/src/AnyAndUnknown.ts
@@ -1,4 +1,4 @@
-let List : any ;
+let List: any;
 
 List = 12;
 List = 3.4;
@@ -8,13 +8,45 @@ List.toString();
 
 
 
-let topList : unknown;
+let topList: unknown;
 topList = 12;
 topList = 3.4;
 topList = "List of number";
 topList = true;
 //topList.toString();// it throw error 
 
-if(typeof topList === "string"){
+if (typeof topList === "string") {
     topList.toString();
+}
+
+
+try {
+
+} catch (error) {
+
+    if (error instanceof Error) {
+        console.error(error.message)
+
+    }
+    //console.error(error.message)  //[it will throw error => it not sure the error has a message]
+
+}
+
+type Role = "Admin" | "User";
+
+function redirectBasedOnRole(role :Role) :void{
+    if(role === "Admin"){
+        console.log(`Redirectiong to the admin dashbord`);
+        return
+    }
+    if(role === "User"){
+        console.log(`Redirectiong to the User dashbord`);
+        return
+    }
+    role;
+
+}
+
+function neverReturn() : never {
+    while(true) {}
 }
