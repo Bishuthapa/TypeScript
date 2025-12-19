705d503dddb80e6a4ef9773b20cb0eeb9f98824f
Bishu Thapa <thapabishu435@gmail.com>
2025-12-19 20:24:45 +0545

    Interface

--- ts/src/TypeAndInterface.ts
+++ ts/src/TypeAndInterface.ts
@@ -1,5 +1,23 @@
-type Movies = "drama" | "action" | "fiction";
+// type Movies = "drama" | "action" | "fiction";
 
-function Watch(m : Movies){
-    console.log(m);
+// function Watch(m : Movies){
+//     console.log(m);
+// }
+
+
+interface Person {
+    firstName : string,
+    lastName : string
 }
+
+
+function getFullName(n : Person){
+    return `${n.firstName} ${n.lastName}`
+}
+
+let Joshan = {
+    firstName : "Joshan",
+    lastName : "Leo"
+}
+
+console.log(getFullName(Joshan))
