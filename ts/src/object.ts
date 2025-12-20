8ff0b3a517f1dc55ee5a237224dd9160a291cce1
Bishu Thapa <thapabishu435@gmail.com>
2025-12-20 20:56:56 +0545

    objects

--- ts/src/object.ts
+++ ts/src/object.ts
@@ -8,3 +8,15 @@ pen = {
     name : "Nataraj",
     price : 25
 }
+
+type Shoes = {
+    name : string,
+    price : number,
+    brand : string
+}
+
+const MenShoes: Shoes = {
+    name : "Samba",
+    price : 2699,
+    brand: "Adidas"
+}
