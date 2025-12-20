6399b7bb91d3432f61f2b0ab3896f26c78466c8f
Bishu Thapa <thapabishu435@gmail.com>
2025-12-20 22:55:04 +0545

    function in typescript

---
+++ ts/src/functions.ts
@@ -0,0 +1,13 @@
+function Drinking(type : string, times: number) {
+ 
+    console.log(`He is drinking ${type} more than ${times} times.`);
+}
+
+Drinking("Water", 6);
+
+function hey() : void { /// return type is void
+ 
+    console.log("Hi");
+}
+
+hey();
