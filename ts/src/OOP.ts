8ce3d4127cd7728a61342b660223292e2ddc4958
Bishu Thapa <thapabishu435@gmail.com>
2025-12-21 13:45:27 +0545

    object oriented program using ts

--- ts/src/OOP.ts
+++ ts/src/OOP.ts
@@ -9,3 +9,31 @@ class Book {
 }
 
 const MyBook = new Book("Comedy", 2000);
+
+class Shop {
+    protected shopName = "tea shop"
+}
+
+class Branch extends Shop {
+    getName(){
+        return this.shopName;
+    }
+}
+
+new Branch().getName;
+
+class Pen {
+    private _Ink = 10;
+
+    get Ink() {
+        return this._Ink
+    }
+
+    set Ink(value :number){
+        if(value > 10) throw new Error("Too thick");
+        this._Ink = value;
+    }
+}
+
+const C = new Pen()
+C.Ink = 6;
