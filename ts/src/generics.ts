16036bd924ffe42781742e24cd981680dde9b18a
Bishu Thapa <thapabishu435@gmail.com>
2025-12-21 14:42:48 +0545

    generics

--- ts/src/generics.ts
+++ ts/src/generics.ts
@@ -5,3 +5,29 @@ function WrapArray<T>(item: T ) : T[]{
 WrapArray("Choclate");
 WrapArray(55);
 WrapArray({flavor: "Coconut"})
+
+
+function pair<A, B>(a:A ,b:B): [A,B]{
+    return [a, b]
+} 
+
+pair("hancy", "bro");
+
+
+interface Box<T> {
+    content :T
+}
+
+
+
+
+
+interface ApiPromise<T>{
+    status: number,
+    data : T
+}
+
+const res: ApiPromise<{flavor: string}> ={
+    status :200,
+    data : {flavor: "candy"}
+}
